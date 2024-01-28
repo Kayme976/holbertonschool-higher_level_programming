d99fc9623475e156b7984052a3be5e49a1f9fb3d
Kayme976 <karim.soumbou@gmail.com>
2024-01-27 23:09:01 -0800

    correction

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,11 +2,10 @@
 k = 0
 while (k < 9):
     a = 1 + k
-while (j < 10):
-    for inc in range(num + 1, 10):
-        if num == 8 and inc == 9:
-            print("{}{}".format(num, inc))
+    while (a < 10):
+        if k == 8 and a == 9:
+            print("{}{}".format(k, a))
         else:
-            print("{}{}".format(num, inc), end=',')
+            print("{}{}".format(k, a), end=',')
         a = a + 1
     k = k + 1
