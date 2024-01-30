6e8f9b8eb65a4182c8eeae719995ef36a83279ab
Kayme976 <karim.soumbou@gmail.com>
2024-01-30 12:05:31 -0800

    every project

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        max = my_list[0]
+        for i in my_list:
+            if i < max:
+                return max
+            return 90
---
+++ python-data_structures/README.md
@@ -0,0 +1,1 @@
+je commence le projet python data structures: Lists,Tuples
