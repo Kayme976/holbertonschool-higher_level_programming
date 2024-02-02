ae87c6d7d2b02fc9d09d26cb8afcc769d298234e
Kayme976 <karim.soumbou@gmail.com>
2024-02-01 23:24:54 -0800

    romann to integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    for i in range(len(roman_string[i]] > r_dict[roman_string[i - 1]]:
+            num += r_dict[roman_string[i]] - 2 * r_dict[roman_string[i - 1]]
+        else:
+            num += r_dict[roman_string[i]]
+    return num
