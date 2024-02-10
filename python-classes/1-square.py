6e20d295d6771879dd0ce8bf51e70777b18bf2c8
Kayme976 <karim.soumbou@gmail.com>
2024-02-10 13:06:39 -0800

    update

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""module defines a class representing a square
+"""this module defines a class reprenting a square.
 """
 
 
+
 class Square:
-    """defines a square
+    """this class that defines a square.
     """
     __size = None
 
-    def __int__(self, __size):
+    def __init__(self, __size):
         if isinstance(__size, int):
             self.__size = __size
         else:
