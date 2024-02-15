3033bac8c844161453080c7dfc4b9c66c05d8d33
Kayme976 <karim.soumbou@gmail.com>
2024-02-15 07:45:04 -0800

    obliger

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines a class checking function."""
+
+
+def is_same_class(obj, a_class):
+    """check if  an object is exactly an instance of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == a_class:
+        return True
+    return False 
