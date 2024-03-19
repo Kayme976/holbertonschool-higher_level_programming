ab8b2c449a277617bf56da21e66a91329ed24ff0
Kayme976 <7508@holbertonstudents.com>
2024-03-19 15:30:33 +0100

    Fix capitalization in table creation comment

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- Creates  the table id_not_null on your MySQL server
+-- creates  the table id_not_null on your MySQL server
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
