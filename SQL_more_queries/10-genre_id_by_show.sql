e543260ac5c9d554af09d98a20536bcfc4983d9e
Kayme976 <7508@holbertonstudents.com>
2024-03-19 16:46:48 +0100

    le (s)

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Import the database dump from hbtn_0d_tvshows
 SELECT s.title, g.genre_id
-FROM tv_show s
+FROM tv_shows s
 INNER JOIN tv_show_genres g
 ON s.id = g.show_id
 ORDER BY s.title, g.genre_id ASC;
