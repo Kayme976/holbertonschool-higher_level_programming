224d6672a09b617abfa06ef4e8ce6b637269ed0a
Kayme976 <7508@holbertonstudents.com>
2024-03-19 01:50:30 +0100

    Refactor SQL table creation query

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Creates  the table force_name on your MySQL server
-CREATE TABLE IF NOT EXISTS $1.force_name (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
