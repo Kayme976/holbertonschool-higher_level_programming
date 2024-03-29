4cd3b334403b449f0e979a835ae26db807ccfb3a
Kayme976 <7508@holbertonstudents.com>
2024-03-29 15:50:10 +0100

    Remove unused imports and add variable_load_5 import in 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import statistics
-import hidden_4
-
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if name[:2] != "__":
-            print(name)
+    import variable_load_5
+    print("{}".format(variable_load_5.a))
