72896d80fc979dc5113d5bbc8b613b40e0e34e7f
Kayme976 <7508@holbertonstudents.com>
2024-03-29 15:05:58 +0100

    Refactor code to improve readability and follow PEP8 guidelines

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,17 +7,21 @@ database `hbtn_0e_0_usa`.
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
-    rows = cursor.fetchall()
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
 
-    for row in rows:
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    for row in cursor.fetchall():
         print(row)
+
+    cursor.close()
+    database.close()
