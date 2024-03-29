d21e72f0ca28f1b923393cab2a92e013a9bae45f
Kayme976 <7508@holbertonstudents.com>
2024-03-29 14:36:08 +0100

    Add Python scripts for manipulating State objects in the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
+"""
+Lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-def list_states(username, password, database):
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states`")
+    rows = cursor.fetchall()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    states = cursor.fetchall()
-    
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
+    for row in rows:
+        print(row)
