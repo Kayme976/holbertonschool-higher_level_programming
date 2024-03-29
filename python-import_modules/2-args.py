b5ab464202054ae5bdd2d434a6b2729257004a2c
Kayme976 <7508@holbertonstudents.com>
2024-03-29 13:36:41 +0100

    Refactor code for handling command line arguments and module import

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/use/bin/python3
 if __name__ == "__main__":
-    import sys
-    len = len(sys.argv)
-    if len == 2:
-        print("{} argument:".format(len - 1))
-    if len == 1:
-        print("{} arguments.".format(len - 1))
-    if len > 2:
-        print("{} arguments:".format(len - 1))
-    for i in range(1, len):
-        print("{}: {}".format(i, sys.argv[i]))
+    num_args = len(sys.argv) - 1
+    if num_args == 1:
+        print("1 argument:")
+    elif num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
